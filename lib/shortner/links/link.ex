c844aaa33508befbffac89107e1ab7b96d784a94
M Braga <mateus.braga@rebase.com.br>
2020-01-05 19:33:22 -0300

    redirecting and saving

--- lib/shortner/links/link.ex
+++ lib/shortner/links/link.ex
@@ -1,4 +1,4 @@
-defmodule Shortner.Links.Links do
+defmodule Shortner.Links.Link do
   use Ecto.Schema
   import Ecto.Changeset
 
--- lib/shortner_web/controllers/url_controller.ex
+++ lib/shortner_web/controllers/url_controller.ex
@@ -1,21 +1,23 @@
 defmodule ShortnerWeb.UrlController do
   use ShortnerWeb, :controller
 
-  alias Shortner.Links.Links
+  alias Shortner.Links.Link
   alias Shortner.Repo
   def index(conn, _params) do
-    changeset = Links.changeset(%Links{})
+    changeset = Link.changeset(%Link{})
+    links = Repo.all(Link)
     render(conn, "index.html", changeset: changeset)
   end
 
-  def create(conn, %{"links" => %{"url" => url}}) do
-    link = %Links{ url: url, shortned: Links.short_link }
-    changeset = Links.changeset(link)
+  def create(conn, %{"link" => %{"url" => url}}) do
+    link = %Link{ url: url, shortned: Link.short_link }
+    changeset = Link.changeset(link)
     
     case Repo.insert(changeset) do
-      { :ok, _struct } -> 
+      { :ok, link } -> 
         conn
-        |> put_flash(:info, "URL encurtada com sucesso")
+        |> put_flash(:info, "URL encurtada com sucesso\n 
+        utilize /#{link.shortned} para redirecionamento")
         |> redirect(to: Routes.url_path(conn, :index))
       { :error, changeset } ->
         render(conn,"index.html", changeset: changeset)
@@ -23,14 +25,12 @@ defmodule ShortnerWeb.UrlController do
   end
 
   def show(conn, %{ "url" => url }) do
-    case Repo.get_by(Shortner.Links.Links, shortned: url) do
+    case Repo.get_by(Link, shortned: url) do
       :nil -> 
         conn
         |> put_flash(:error, "Url não encontrada")
         |> redirect(to: Routes.url_path(conn, :index))
-      link -> 
-        # link.url |> url = Shortner.Links.Links.check_url
-        redirect(conn, external: Links.check_url(link.url))
+      link -> redirect(conn, external: Link.check_url(link.url))
     end
   end
 
